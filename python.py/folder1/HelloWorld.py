6597da426c778307c877519e3dd67f81fd06e045
Perseptious907 <82676053+Perseptious907@users.noreply.github.com>
2021-05-05 15:06:50 -0800

    Add files via upload

---
+++ python.py/folder1/HelloWorld.py
@@ -0,0 +1,32 @@
+def setCred():
+    username = input("create new username: ")
+    password = input("create new password: ")
+
+setCred()
+print (username)
+print (password)
+
+def lifeIsGood():
+    print('you are in')
+
+def username():                             #username function
+    confirm = input ('Username: ')  #this is my username variable
+
+    if (confirm == 'username'):   #if this reads yes then it lets you in
+        password()
+    elif (confirm != 'username'):       #if it is no then it does not and repeats
+        print('try again')
+        
+
+def password():                     #password function
+    confirm_1 = input('Password: ')     #this is my password variable
+
+    if (confirm_1 == 'password'):   #if this reads yes then it lets you in
+        lifeIsGood()
+    elif (confirm_1 != 'password'):       #if it is no then it does not and repeats
+        print ("try again")
+        
+
+setCred()
+username()
+
