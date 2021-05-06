ce568466deaf4ea1d1d7c60e82b4e9b5c864a612
Perseptious907 <82676053+Perseptious907@users.noreply.github.com>
2021-05-05 21:31:18 -0800

    Update HelloWorld.py

--- python.py/folder1/HelloWorld.py
+++ python.py/folder1/HelloWorld.py
@@ -11,16 +11,16 @@ def lifeIsGood():
 
 def username():                             #username function
     confirm = input ('Username: ')  #this is my username variable
+    
+def password():                     #password function
+    confirm_1 = input('Password: ')     #this is my password variable
+
 
     if (confirm == 'username'):   #if this reads yes then it lets you in
         password()
     elif (confirm != 'username'):       #if it is no then it does not and repeats
         print('try again')
         
-
-def password():                     #password function
-    confirm_1 = input('Password: ')     #this is my password variable
-
     if (confirm_1 == 'password'):   #if this reads yes then it lets you in
         lifeIsGood()
     elif (confirm_1 != 'password'):       #if it is no then it does not and repeats
